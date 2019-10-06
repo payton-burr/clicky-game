9fbc6fa4d560931e8f67edf3e92771a6d2a8497a
Payton Burr <greatpayton@gmail.com>
2019-10-05 18:12:12 -0600

    Updates

---
+++ precache-manifest.8988bdaa26896a8186441ea3026afd21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8320438c7d367e0c556175ce2bff0b09",
+    "url": "/payton-burr/clicky-game/index.html"
+  },
+  {
+    "revision": "96e75dfaf4d9460e5dca",
+    "url": "/payton-burr/clicky-game/static/css/main.147612b7.chunk.css"
+  },
+  {
+    "revision": "4f8f99b3f34a638e1589",
+    "url": "/payton-burr/clicky-game/static/js/2.57f8abea.chunk.js"
+  },
+  {
+    "revision": "96e75dfaf4d9460e5dca",
+    "url": "/payton-burr/clicky-game/static/js/main.9499d9fa.chunk.js"
+  },
+  {
+    "revision": "0ea36a3c941b0e9cab2c",
+    "url": "/payton-burr/clicky-game/static/js/runtime-main.4e3d9eba.js"
+  }
+]);
